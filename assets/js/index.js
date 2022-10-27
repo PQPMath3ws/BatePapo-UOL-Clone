0e16c3caefb7d00a1aac7a51505d6f22e96cf6e5
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-10-26 21:51:57 -0300

    Correção de redirect de rotas no index e remoção de variável fixa do nome no index

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,15 @@
 window.onload = async function () {
     let name = localStorage.getItem("username");
-    name = "LocalHost";
     if (name) {
         const checkUserURL = "https://mock-api.driven.com.br/api/v6/uol/participants";
         try {
             await request("POST", checkUserURL, {name});
-            window.location.href = window.location.origin + window.location.pathname + "chat.html";
+            window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf('/')) + "/" + "chat.html";
         } catch (error) {
             localStorage.removeItem("username");
-            window.location.href = window.location.origin + window.location.pathname + "login.html";
+            window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf('/')) + "/" + "login.html";
         }
     } else {
-        window.location.href = window.location.origin + window.location.pathname + "login.html";
+        window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf('/')) + "/" + "login.html";
     }
 };
