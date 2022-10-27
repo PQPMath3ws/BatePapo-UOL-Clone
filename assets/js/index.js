e655197c09e8fbe85b01c64fe2c417fc01de7832
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-10-26 22:35:16 -0300

    Implementado nova forma de request

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,19 @@
 window.onload = async function () {
     let name = localStorage.getItem("username");
     if (name) {
-        const checkUserURL = "https://mock-api.driven.com.br/api/v6/uol/participants";
-        try {
-            await request("POST", checkUserURL, {name});
+        const checkUserURL = "https://mock-api.driven.com.br/api/v6/uol/status";
+        const request = await fetch(checkUserURL, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: `${name}`
+            })
+        });
+        if (request.ok) {
             window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf('/')) + "/" + "chat.html";
-        } catch (error) {
+        } else {
             localStorage.removeItem("username");
             window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf('/')) + "/" + "login.html";
         }
